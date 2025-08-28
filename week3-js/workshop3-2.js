7e1d7936e2fdeaa634cec4c7ec008fb06d159dc0
Kittitouchhh <s66111596@gmail.com>
2025-08-28 21:45:41 +0700

    Finish

--- week3-js/workshop3-2.js
+++ week3-js/workshop3-2.js
@@ -1,12 +1,7 @@
 const price = 200
-<<<<<<< HEAD
 
 const Discount = (10/100)                         //ส่วนลดปกติ 5%
 const discountmorethangivehunred = (10/100)      //ส่วนลด 500  10% + 5%
-=======
-const Discount = (10/100)                         //ส่วนลดปกติ 5%
-const discountmorethangivehunred = (10/100)      //ส่วนลด500  10% + 5%
->>>>>>> d2732776eba594c973988dc40b401e693e6a17c4
 
 const bill = price - (price*Discount)  //ราคาปกติ
 const billmorethanfivehunred = price - (bill * discountmorethangivehunred) //ราคามากกว่า500
