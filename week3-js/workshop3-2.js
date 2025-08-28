fc999fb963816a0a672f233079efaaada5aaaa38
Kittitouchhh <s66111596@gmail.com>
2025-08-28 20:44:37 +0700

    Initial commit

---
+++ week3-js/workshop3-2.js
@@ -0,0 +1,17 @@
+const price = 200
+
+const Discount = (10/100)                         //ส่วนลดปกติ 5%
+const discountmorethangivehunred = (10/100)      //ส่วนลด 500  10% + 5%
+
+const bill = price - (price*Discount)  //ราคาปกติ
+const billmorethanfivehunred = price - (bill * discountmorethangivehunred) //ราคามากกว่า500
+
+if (bill <= 0){
+    console.log("price after Discount Percentage : 0 " )
+}
+else if (bill <= 500){
+    console.log("price after Discount Percentage : " + (bill+50)  )
+}
+else if (bill > 500){
+    console.log("price after Discount Percentage : " + bill )
+}
---
+++ week3-js/workshop3-3.js
@@ -0,0 +1,24 @@
+student = [
+    {
+        name : "gus",
+        grade : "A"
+    },
+    {
+        name : "boss",
+        grade : "A"
+    },
+    {
+        name : "Tle son of Tum",
+        grade : "F"
+    }
+]
+
+student.forEach(function(student,index){
+    console.log(`นาย  ${student.name} ได้เกรด : ${student.grade}`);
+})
+
+function showStudentCount (){
+    console.log("")
+    console.log("จำนวนนักเรียน :" +" "+ student.length + " " + "คน")
+}
+showStudentCount()
